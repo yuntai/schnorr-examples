1208aae842f20ad6c4a96a35d55474236cc1d33c
Yuntai Kyong <yuntai.kyong@gmail.com>
2018-04-10 23:32:07 +0900

    add discrete log contract example

---
+++ schnorr/dlc.py
@@ -0,0 +1,83 @@
+# using the codebase which is used in Programming Blockchain seminar(http://programmingblockchain.com/) 
+# The codebase can be found in (https://github.com/jimmysong/pb-exercises)
+
+# Based on 
+# [Discret Log Contracts Paper](https://adiabat.github.io/dlc.pdf)
+
+import sys
+sys.path.append("..")
+import ecc
+from ecc import PrivateKey
+from random import randint
+from helper import double_sha256, little_endian_to_int
+from schnorr import H, HI
+
+def B(v):
+    return v.to_bytes(256//8, 'little')
+
+class Tx:
+    def __init__(self, pub):
+        self.pub = pub
+
+    def redeem(self, s):
+        return PrivateKey(s).point == self.pub
+
+class Agent:
+    def __init__(self):
+        self.pk = PrivateKey(randint(0, 2**256))
+
+    # public point
+    def point(self):
+        return self.pk.point
+
+    # populate Contract Execution Transactions
+    # stored on the agent's computer
+    # using V, R and a range of values
+    def CET(self, V, R, vals):
+        # calculate public key where payout to be sent when winning the bet
+        pubout = lambda m: self.pk.point + R - HI(H(B(m)), R.sec()) * V
+        # txs signed by counterparty
+        self.txs = {m: Tx(pubout(m)) for m in vals}
+
+    # sign & broadcast
+    def redeem(self, s, m):
+        return self.txs[m].redeem((self.pk.secret + s)%ecc.N)
+
+class Oracle: 
+    def __init__(self):
+        self.pk = PrivateKey(randint(0, 2**256))
+
+    # pre-commit R and publish it with its public point V
+    def pre_commit(self):
+        self.k = randint(0, 2**256)
+        self.R = self.k * ecc.G
+        V = self.pk.point
+        return V, self.R
+
+    # announce winner and sign it
+    def endorse_state(self):
+        m = randint(1, 6)
+        v = self.pk.secret
+        s = self.k - HI(H(B(m)), self.R.sec()) * v
+        return m, s
+
+def dlc():
+    olivia = Oracle()
+    V, R = olivia.pre_commit() 
+
+    bob = Agent()
+    alice = Agent()
+
+    bob_bet = [1, 2, 3, 4, 5]
+    bob.CET(V, R, bob_bet)
+    alice_bet = [6]
+    alice.CET(V, R, alice_bet)
+
+    m, s = olivia.endorse_state()
+
+    if m in bob_bet:
+        assert bob.redeem(s, m)
+    else:
+        assert alice.redeem(s, m)
+
+dlc()
